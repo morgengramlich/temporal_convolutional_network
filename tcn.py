4c6953b4be437e636fd4fc6444fd9b0369fefab3
morgengramlich <danila.sergeyev@gmail.com>
2024-04-26 18:42:32 -0500

    Fix TCN architecture

--- tcn.py
+++ tcn.py
@@ -12,53 +12,62 @@ class ResidualBlock(nn.Module):
         self.kernel_size = kernel_size
         self.dilation = dilation
         self.pad1 = nn.ConstantPad1d(((self.kernel_size-1)*dilation, 0), 0.0)
-        self.conv1 = nn.utils.weight_norm(nn.Conv1d(
+        self.conv1 = torch.nn.utils.parametrizations.weight_norm(nn.Conv1d(
             in_channels=self.input_size,
             out_channels=self.hidden_size,
             kernel_size=self.kernel_size,
             dilation=dilation,
         ))
-        self.dropout1 = nn.Dropout(p=0.2)
+        self.dropout1 = nn.Dropout1d(p=0.2)
         self.pad2 = nn.ConstantPad1d(((self.kernel_size-1)*dilation, 0), 0.0)
-        self.conv2 = nn.utils.weight_norm(nn.Conv1d(
+        self.conv2 = torch.nn.utils.parametrizations.weight_norm(nn.Conv1d(
             in_channels=self.hidden_size,
-            out_channels=self.input_size,
+            out_channels=self.hidden_size,
             kernel_size=self.kernel_size,
             dilation=dilation,
         ))
-        self.dropout2 = nn.Dropout(p=0.2)
+        self.dropout2 = nn.Dropout1d(p=0.2)
+        self.identity_conv = None
+        if self.input_size > 1 and self.input_size != self.hidden_size:
+            self.identity_conv = nn.Conv1d(
+                in_channels=self.input_size,
+                out_channels=self.hidden_size,
+                kernel_size=1,
+            )
 
     def forward(self, x):
         '''One step of computation'''
         output = self.pad1(x)
-        output = F.relu(self.conv1(output))
+        output = F.leaky_relu(self.conv1(output))
         output = self.dropout1(output)
         output = self.pad2(output)
-        output = F.relu(self.conv2(output))
+        output = F.leaky_relu(self.conv2(output))
         output = self.dropout2(output)
-        return F.relu(x + output)
+        if self.input_size > 1 and self.input_size != self.hidden_size:
+            x = self.identity_conv(x)
+        return F.leaky_relu(x + output)
 
 class TCN(nn.Module):
     '''Temporal Convolutional Network'''
 
-    def __init__(self, input_size, kernel_sizes, dilations):
+    def __init__(self, input_size, num_filters, kernel_sizes, dilations):
         super(TCN, self).__init__()
+        if len(num_filters) != len(kernel_sizes):
+            raise ValueError('output_sizes and kernel_sizes must be of the same size')
         if len(kernel_sizes) != len(dilations):
             raise ValueError('kernel_sizes and dilations must be of the same size')
         self.input_size = input_size
+        self.num_filters = num_filters
         self.kernel_sizes = kernel_sizes
         self.dilations = dilations
         self.residuals = nn.Sequential()
         for n in range(len(kernel_sizes)):
-            self.residuals.append(ResidualBlock(self.input_size, self.input_size, self.kernel_sizes[n], self.dilations[n]))
-        self.dropout = nn.Dropout(p=0.2)
-        self.fc = nn.Linear(self.input_size, self.input_size)
+            if n == 0:
+                self.residuals.append(ResidualBlock(self.input_size, self.num_filters[n], self.kernel_sizes[n], self.dilations[n]))
+            else:
+                self.residuals.append(ResidualBlock(self.num_filters[n-1], self.num_filters[n], self.kernel_sizes[n], self.dilations[n]))
   
     def forward(self, value):
         '''One step of computation'''
-        output = torch.permute(value, (0, 2, 1))
-        output = self.residuals(output)
-        output = torch.permute(output, (0, 2, 1))
-        output = self.dropout(output)
-        output = self.fc(output)
+        output = self.residuals(value)
         return output
